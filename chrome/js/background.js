781343959673ce3254e30ad9253adc373563fd55
Akhil Jay <ajayapra@akamai.com>
2018-04-01 14:01:46 -0400

    adding akamai pragma headers to the extension

--- chrome/js/background.js
+++ chrome/js/background.js
@@ -1,3 +1,7 @@
+var debug_headers = "akamai-x-get-clientip, akamai-x-cache-on, akamai-x-cache-remote-on, akamai-x-check-cacheable, akamai-x-get-cache-key, akamai-x-get-extracted-values, akamai-x-get-nonces, akamai-x-get-ssl-client-session-id, akamai-x-get-true-cache-key, akamai-x-serial-no, akamai-x-feo-trace, akamai-x-get-request-id"
+var akamai_header = { "name":"Pragma", "value":debug_headers}
+
+
 var TabCompleteNotifier = {
     tabs: {},
     add: function(tabId, callback) {
@@ -117,3 +121,23 @@ var jsCodeUtilities = function(code) {
 
 chrome.runtime.onMessage.addListener(onMessageListener);
 chrome.tabs.onUpdated.addListener(onUpdatedListener);
+
+
+chrome.webRequest.onBeforeSendHeaders.addListener(
+  function(details) {
+    console.log("Akamai headers added to: " + details.url);
+    var pragma_exists = false
+    for (var i = 0; i < details.requestHeaders.length; ++i) {
+      if (details.requestHeaders[i].name === 'Pragma') {
+        details.requestHeaders[i].value = details.requestHeaders[i].value.concat(", ",debug_headers)
+        pragma_exists = true
+        break;
+      }
+    }
+    if(!pragma_exists) {
+      details.requestHeaders.push(akamai_header)
+    }
+    return {requestHeaders: details.requestHeaders};
+  },
+  {urls: ["<all_urls>"]},
+  ["blocking", "requestHeaders"]);
