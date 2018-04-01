faf51d8927eb80083d8cd0505cb1b2da1870ee99
Akhil Jay <ajayapra@akamai.com>
2018-03-31 23:54:28 -0400

    GA local scripts

---
+++ chrome/js/vendors/ga.js
@@ -0,0 +1,14 @@
+var _AnalyticsCode = 'UA-116652320-1';
+
+
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', _AnalyticsCode]);
+_gaq.push(['_trackPageview']);
+(function() {
+  var ga = document.createElement('script');
+  ga.type = 'text/javascript';
+  ga.async = true;
+  ga.src = 'js/vendors/gasrc.js';
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(ga, s);
+})();
